8d0a62760ae50e51ed31cfb028dc40615ad47bab
LanJiang <30459778+Lan-Jiang@users.noreply.github.com>
2022-01-29 22:47:59 -0800

    WEB501 LAB Wk4 Day3 added show/hide discription js finished

--- js/gallery.js
+++ js/gallery.js
@@ -3,9 +3,9 @@
 // var photoList = ["dog1", "dog2", "dog3", "dog4", "dog5"];
 var captionTexts = ["Vince, 16 months", "Teddy, 3 months", "Victor, 5 months",
                         "Uni, 9 months", "Meatball, 12 months"
-                        ,"Meme, 3 years" , "Flur, 1 year", "Lia, 14 months", "Cath, 9 months", "CC, 16 months  " ]
+                        ,"Meme, 3 years" , "Flur, 1 year", "Lia, 14 months", "Cath, 9 months", "CC, 16 months  " ];
 var descTexts = ["Vince likes treats", "Teddy likes play", "Victor likes hugging", "Uni likes sea", "Meatball likes sunshine"
-                        ,"Meme likes wool ball", "Flur likes cans", "Lia likes snow", "Cath likes apple", "CC likes napping"]
+                        ,"Meme likes wool ball", "Flur likes cans", "Lia likes snow", "Cath likes apple", "CC likes napping"];
 var photos = [];
 var fileNames = [];
 var imagefileNames = [];
@@ -29,7 +29,6 @@ var infoTexts = ["Vince is active and he likes the red color, he likes the sea."
                     , "Lia is orange and she likes taking naps."
                     , "Cath is white and she has beautiful eyes."
                     , "CC is cautious and she is calm."];
-
 var closeinfoBox = "Click to close";
 var openDescTag = "<p class='description' onClick='showinfoBox(";
 var openDescClose = ")'>";
@@ -41,14 +40,14 @@ for (var i=0; i<10; i++) {
         fileNames.push("dog" + [i+1]);
         photos.push("<img src = 'images/" +fileNames[i]+ ".jpeg' alt=''>");//  
         captions.push(openCaptionTag + captionTexts[i] + closeCaptionTag);
-        descriptions.push(openDescTag + descTexts[i] + closeDescTag);  
+        descriptions.push(openDescTag + [i] + openDescClose + descTexts[i] + closeDescTag);  
         image = openList + descriptions[i] + photos[i] + captions[i] + closeList;        
         imageList.push(image);
     } else if (i>=5 && i<10) {
         fileNames.push("cat" + [i-4]);
         photos.push("<img src = 'images/" +fileNames[i]+ ".jpeg' alt=''>");//  
         captions.push(openCaptionTag + captionTexts[i] + closeCaptionTag);
-        descriptions.push(openDescTag + descTexts[i] + closeDescTag);  
+        descriptions.push(openDescTag + [i] + openDescClose + descTexts[i] + closeDescTag);  
         image = openList + descriptions[i] + photos[i] + captions[i] + closeList;        
         imageList.push(image);
     }
@@ -60,7 +59,6 @@ document.getElementById("photos").innerHTML = imageList.join(" ");
 //  End photo gallery js
 
 //  Start infoBox-->
-
 function showinfoBox(i) {
     document.getElementById("infoBox").style.visibility = 'visible';
     document.getElementById("title").innerHTML = titleTexts[i];
