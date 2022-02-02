e76c397123e9091f3afcb33e4a526195b573d68b
LanJiang <30459778+Lan-Jiang@users.noreply.github.com>
2022-02-01 21:59:37 -0700

    WEB501 HWK Wk4 Day3 dropdown js function finished

--- js/contactbox.js
+++ js/contactbox.js
@@ -1,8 +1,3 @@
-//Hide contactBox when load page
-function hidecontactBox() {
-    document.getElementById("contactBox").style.visibility = 'hidden';
-};
-
 //Create js for contact box show/hide
 var labelTexts = ["Enter your email", "Enter your phone"];
 var inputTexts = ["type = 'email' name = 'email'", "type = 'text' name = 'phone'"];
@@ -17,25 +12,38 @@ for (i=0; i<=1; i++) {
 }
 //console.log(contactBoxList);
 
-//Need your help here, how to call the option and so that when click can pop up the content.
-//Thanks!
-var option = document.getElementById("contactMethod");
-option.addEventListener("click", evaluateOption());
-
+//Not idea way, seeking TA's help on improving the code and method;
 function evaluateOption() {
-    optionvalue = document.getElementById("contactMethod").value;
-    if (option == 'email') {
+    var select = document.getElementById('contactMethod');
+    var option = select.options[select.selectedIndex];
+    optionvalue = option.value;
+    if (optionvalue == 'email') {
+        document.getElementById("contactBox").style.visibility = 'visible';
         enterContext(0);
-    } else if (option == 'phone') {
+    } else if (optionvalue == 'phone') {
+        document.getElementById("contactBox").style.visibility = 'visible';
         enterContext(1);
+    } else if (optionvalue == 'selectone') {
+        document.getElementById("selectone").style.display= 'none';
+        document.getElementById("contactBox").style.visibility = 'hidden'; // hidden box when click Select One;
     }
 };
+evaluateOption();
+
+// //Hide contactBox when load page
+// function hidecontactBox() {
+//     document.getElementById("contactBox").style.visibility = 'hidden';
+// };
+// hidecontactBox();
 
 function enterContext(i) {
     document.getElementById("contactBox").innerHTML = contactBoxList[i];
 };
 
 enterContext();
+
+
+
 
 
 
