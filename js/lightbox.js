749d189e8e43e484510d01b124b5012fe7ce0a46
LanJiang <30459778+Lan-Jiang@users.noreply.github.com>
2022-02-05 18:47:35 -0700

    WEB501 LAB Wk5 D3 added lightbox finished

--- js/lightbox.js
+++ js/lightbox.js
@@ -8,23 +8,16 @@ $(document).ready(function(){
         //Check if lightbox has an image
         if ($('.box').contents('img')) {
             $('.box').contents().remove('img'); //If true, clear image
-        }
+        };
 
         //Get text content in attribute
-        //var $altvalue = $(this).attr('alt'); //or var altvalue = $(this).attr('alt');
-        var $altvalue = $(this).attr('alt');
-        //$('.box').append($altvalue);
-
-        var $imgsrc = $(this).attr('src')
+        var $imgsrc = $(this).attr('src');
         $('.box').append("<img src='" + $imgsrc + "'>");
-        // if ($altvalue=="dog1") {
-        //     //var img = $('#photos:nth-child(1) img').clone(); //Duplicate DOM element
-        //     $('.box').append('<img src="images/dog1.jpeg" alt="">'); //Insert duplicated element in another element
-        // }
+
     });
 
     /* Click to close lightbox */
-    $('.close, .backdrop').click(function(){
+    $('.close').click(function(){
         $('#photos').animate({'opacity':'1'}, 300, 'linear', function(){
             //$('.backdrop').css('display', 'none');
         });
